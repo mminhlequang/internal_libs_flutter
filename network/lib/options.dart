2b21bea9d5029a586671f0e60a596e8fd5b07aca
Minh Le Quang <>
2025-07-28 09:26:53 +0700

    fix

--- network/lib/options.dart
+++ network/lib/options.dart
@@ -13,7 +13,7 @@ const String methodDelete = "DELETE";
 
 var appHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Credentials": true,
+  "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Headers":
       "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
   "Access-Control-Allow-Methods": "POST, OPTIONS"
